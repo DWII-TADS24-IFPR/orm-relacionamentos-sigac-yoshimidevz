9593fb6ed3f1bde4e301badb411b2ef08b5c3710
yoshimidevz <yoshimidev1@gmail.com>
2025-04-23 10:41:47 -0300

    CONEXÃO COM BANCO OK

--- sigac/app/Models/Aluno.php
+++ sigac/app/Models/Aluno.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Role;
 
 class Aluno extends Model
 {
-    //
+    protected $table = 'alunos';
+    protected $fillable = [
+        'nome',
+        'cpf',
+        'email',
+    ];
+
+    function roles(){
+        return $this->belongstoMany(Role::class)->withTimestamps();
+    }
 }
---
+++ sigac/app/Models/Role.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    protected $table = 'roles';
+    protected $fillable = [
+        'titulo',
+    ];
+
+    function alunos(){
+        return $this->belongstoMany(Aluno::class)->withTimestamps();
+    }
+}
